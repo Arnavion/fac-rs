5ca6fc6b993f7d6f05f83309de1d4bdd728ae3e6
Arnavion <arnavion@gmail.com>
2016-10-15 22:02:01 -0700

    Fixed error names and sorted alphabetically.

--- factorio-mods-api/src/error.rs
+++ factorio-mods-api/src/error.rs
@@ -1,8 +1,8 @@
 error_chain! {
 	foreign_links {
 		::hyper::Error, Hyper;
-		::url::ParseError, Parse;
 		::serde_json::Error, JSON;
+		::url::ParseError, Parse;
 	}
 
 	errors {
--- factorio-mods-local/src/error.rs
+++ factorio-mods-local/src/error.rs
@@ -1,17 +1,16 @@
 error_chain! {
 	foreign_links {
-		::glob::PatternError, Pattern;
 		::glob::GlobError, Glob;
-		::std::io::Error, ModListFile;
-		::serde_json::Error, ModListJson;
-		::zip::result::ZipError, BadZippedMod;
+		::std::io::Error, IO;
+		::serde_json::Error, Json;
+		::glob::PatternError, Pattern;
+		::zip::result::ZipError, Zip;
 	}
 
 	errors {
-		Utf8Path(glob_pattern: ::std::path::PathBuf) { }
-		WritePath { }
-		ModList(expected_path: ::std::path::PathBuf) { }
 		EmptyZippedMod(path: ::std::path::PathBuf) { }
 		UnknownModFormat { }
+		Utf8Path(glob_pattern: ::std::path::PathBuf) { }
+		WritePath { }
 	}
 }
