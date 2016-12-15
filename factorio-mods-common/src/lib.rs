5f1e9b690fa703c51f59ac646b98833878ce7b71
Arnavion <arnavion@gmail.com>
2016-12-15 01:36:48 -0800

    Parse dependencies from info.json

--- factorio-mods-common/src/lib.rs
+++ factorio-mods-common/src/lib.rs
@@ -10,10 +10,14 @@ extern crate derive_struct;
 extern crate itertools;
 #[macro_use]
 extern crate lazy_static;
+extern crate regex;
 extern crate semver;
+#[macro_use]
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
+#[cfg(test)]
+extern crate serde_json;
 
 mod types;
 pub use types::*;
--- factorio-mods-common/src/types.rs
+++ factorio-mods-common/src/types.rs
@@ -69,6 +69,40 @@ pub struct ModInfo {
 
 	/// The URL of the homepage of the mod release.
 	homepage: Option<Url>,
+
+	/// Dependencies
+	#[serde(default = "default_dependencies")]
+	#[serde(deserialize_with = "::deserialize_string_or_seq_string")]
+	dependencies: Vec<Dependency>,
+}
+
+/// The specification of a dependency in a mod's info.json
+#[derive(Clone, Debug, PartialEq, new, getters)]
+pub struct Dependency {
+	/// The name of the dependency.
+	name: ModName,
+
+	/// The version of the dependency.
+	version: ::semver::VersionReq,
+
+	/// Whether the dependency is required or not.
+	required: bool,
+}
+
+impl ::serde::Deserialize for Dependency {
+	fn deserialize<D>(deserializer: &mut D) -> Result<Self, D::Error> where D: ::serde::Deserializer {
+		struct Visitor;
+
+		impl ::serde::de::Visitor for Visitor {
+			type Value = Dependency;
+
+			fn visit_str<E>(&mut self, v: &str) -> Result<Self::Value, E> where E: ::serde::Error {
+				parse_dependency(v)
+			}
+		}
+
+		deserializer.deserialize(Visitor)
+	}
 }
 
 lazy_static! {
@@ -124,6 +158,41 @@ pub fn fixup_version(s: &str) -> String {
 	}), ".")
 }
 
+/// The default dependencies of a mod.
+///
+/// Used as the default value of the `dependencies` field in a mod's `info.json` if the field doesn't exist.
+fn default_dependencies() -> Vec<Dependency> {
+	DEFAULT_DEPENDENCIES.clone()
+}
+
+lazy_static! {
+	static ref DEPENDENCY_REGEX: ::regex::Regex = ::regex::Regex::new(r"^(\??)\s*([^<>=]+?)\s*((<|<=|=|>=|>)\s*([\d\.]+))?\s*$").unwrap();
+	static ref DEFAULT_DEPENDENCIES: Vec<Dependency> = vec![Dependency { name: ModName("base".to_string()), version: ::semver::VersionReq::any(), required: true, }];
+}
+
+/// Parses the given string as a Dependency
+fn parse_dependency<E>(s: &str) -> Result<Dependency, E> where E: ::serde::Error {
+	let captures = DEPENDENCY_REGEX.captures(s)
+		.ok_or_else(|| ::serde::Error::invalid_value(&format!("Invalid dependency format {}", s)))?;
+
+	let required = captures[1].is_empty();
+
+	let name = ModName::new(captures[2].to_string());
+
+	let version_req_string = captures.at(3).unwrap_or("*");
+	let version_req =
+		if let Ok(version_req) = parse_version_req(version_req_string) {
+			version_req
+		}
+		else {
+			let fixed_version = captures[4].to_string() + &fixup_version(&captures[5]);
+			::semver::VersionReq::parse(&fixed_version)
+				.map_err(|err| ::serde::Error::invalid_value(::std::error::Error::description(&err)))?
+		};
+
+	Ok(Dependency { name: ModName(name.to_string()), version: version_req, required: required, })
+}
+
 #[cfg(test)]
 mod tests {
 	use super::*;
@@ -141,4 +210,22 @@ mod tests {
 		test_fixup_version_inner("0.14.00", "0.14.0");
 	}
 
+	fn test_parse_dependency_inner(s: &str, name: &str, version: &str, required: bool) {
+		let result = parse_dependency::<::serde_json::error::Error>(s).unwrap();
+		assert_eq!(&**result.name(), name);
+		assert_eq!(result.version(), &::semver::VersionReq::parse(version).unwrap());
+		assert_eq!(result.required(), &required);
+	}
+
+	#[test]
+	fn test_parse_dependency() {
+		test_parse_dependency_inner("base", "base", "*", true);
+		test_parse_dependency_inner("? base", "base", "*", false);
+		test_parse_dependency_inner("?base", "base", "*", false);
+		test_parse_dependency_inner("base >= 0.14.0", "base", ">=0.14.0", true);
+		test_parse_dependency_inner("? base >= 0.14.0", "base", ">=0.14.0", false);
+		test_parse_dependency_inner("base >= 0.14.00", "base", ">=0.14.0", true);
+		test_parse_dependency_inner("some name with spaces >= 1.2.3", "some name with spaces", ">=1.2.3", true);
+		test_parse_dependency_inner("? some name with spaces >= 1.2.3", "some name with spaces", ">=1.2.3", false);
+	}
 }
